181d238deb6c8ceb531beb84444dd27b20997200
Jakub Ciszek <jakubciszek64@gmail.com>
2022-04-04 14:15:01 +0200

    Upscaling model and training

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-import torch
+import pytorch_lightning as pl
 
-print(torch.cuda.is_available())
+from super_resolution.config import RANDOM_SEED, TrainingConfig
+from super_resolution.data import DataClass, ImageModule
+from super_resolution.model import SRScalingModel
+
+pl.seed_everything(RANDOM_SEED)
+
+trainer = pl.Trainer(gpus=1, max_epochs=TrainingConfig.max_epochs)
+data_module = ImageModule(DataClass.DOG, DataClass.DOG)
+model = SRScalingModel()
+
+trainer.fit(model, data_module)
--- super_resolution/model.py
+++ super_resolution/model.py
@@ -1,4 +1,9 @@
 import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from .config import TrainingConfig
 
 
 class SREnhancingModel(pl.LightningModule):
@@ -14,5 +19,45 @@ class SRScalingModel(pl.LightningModule):
     def __init__(self) -> None:
         super().__init__()
 
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 128, kernel_size=3, stride=1, padding=1),
+            nn.PixelShuffle(2),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.PixelShuffle(2),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
+            nn.PixelShuffle(2),
+            nn.ReLU(),
+            nn.Conv2d(8, 3, kernel_size=3, stride=1, padding=1),
+        )
 
-# Img
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.model(x)
+
+    def _unpack_batch(
+        self, batch: tuple[torch.Tensor, torch.Tensor]
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        X, y = batch
+        return X.float(), y.float()
+
+    def training_step(
+        self, batch: tuple[torch.Tensor, torch.Tensor], batch_idx: int
+    ) -> dict:
+        X, y = self._unpack_batch(batch)
+        y_hat = self(X)
+
+        loss = F.mse_loss(y_hat, y)
+        return {"loss": loss}
+
+    def validation_step(
+        self, batch: tuple[torch.Tensor, torch.Tensor], batch_idx: int
+    ) -> None:
+        X, y = self._unpack_batch(batch)
+        y_hat = self(X)
+
+        loss = F.mse_loss(y_hat, y)
+        self.log("val_loss", loss, on_step=True, prog_bar=True)
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=TrainingConfig.lr)
